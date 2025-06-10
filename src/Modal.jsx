ef3a95345503771f4c647c21b99fa1a01876c828
creativeloc <creativeloc@yahoo.com>
2025-06-10 06:27:29 -0400

    setup modal jsx with no css as first step

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,4 +1,18 @@
+import { useGlobalContext } from "./context"
+import { FaTimes } from "react-icons/fa"
+
 const Modal = () => {
-  return <div>Modal</div>
+  const { isModalOpen, closeModal } = useGlobalContext()
+
+  return (
+    <div className="modal-overlay">
+      <div className="modal-container">
+        <h3>Modal Content</h3>
+        <button type="button" className="close-modal-btn" onClick={closeModal}>
+          <FaTimes />
+        </button>
+      </div>
+    </div>
+  )
 }
 export default Modal
