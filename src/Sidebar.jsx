332339df066fc35fe075466a0e6009957ddd1600
creativeloc <creativeloc@yahoo.com>
2025-06-12 06:28:24 -0400

    create sidebar structure without css

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,4 +1,44 @@
+import logo from "./logo.svg"
+import { links, social } from "./data"
+import { FaTimes } from "react-icons/fa"
+
+import { useGlobalContext } from "./context"
+
 const Sidebar = () => {
-  return <div>Sidebar</div>
+  const { isSidebarOpen, closeSidebar } = useGlobalContext()
+
+  return (
+    <aside className="sidebar">
+      <div className="sidebar-header">
+        <img src={logo} alt="Coding Addict" className="logo" />
+        <button type="button" className="close-btn" onClick={closeSidebar}>
+          <FaTimes />
+        </button>
+      </div>
+      <ul className="links">
+        {links.map((link) => {
+          const { id, url, text, icon } = link
+          return (
+            <li key={id}>
+              <a href={url}>
+                {icon}
+                {text}
+              </a>
+            </li>
+          )
+        })}
+      </ul>
+      <ul className="social-links">
+        {social.map((link) => {
+          const { id, url, icon } = link
+          return (
+            <li key={id}>
+              <a href={url}>{icon}</a>
+            </li>
+          )
+        })}
+      </ul>
+    </aside>
+  )
 }
 export default Sidebar
